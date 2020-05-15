cc7af619ba15dded1150b0044bdd8fa8098374c0
cmgospod <cmgospod@users.noreply.github.com>
2020-05-15 15:02:59 -0500

    Reading data

--- playaround.py
+++ playaround.py
@@ -1,3 +1,13 @@
 import pandas as pd
 
 items = pd.read_csv('items.csv')
+print(items.shape)
+
+critic = pd.read_csv('critic.csv')
+print(critic.shape)
+
+user_reviews = pd.read_csv('user_reviews.csv')
+print(user_reviews.shape)
+
+villagers = pd.read_csv('villagers.csv')
+print(villagers.shape)
